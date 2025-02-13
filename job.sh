518a20ca705daa46e506e28759d55d39e7de8335
Santiago Salazar Jaramillo <santiago.salazar-jaramillo@scc.physik.uni-konstanz.de>
2025-02-13 15:22:31 +0100

    added a collection method at the end of each for loop to improve performance and started generating data

--- job.sh
+++ job.sh
@@ -7,9 +7,8 @@
 # folder MUST EXIST!
 #$ -e logs/ # give absolut path to the desired location
 #$ -o logs/
-#$ -l h_vmem=500G
-
+#$ -l h_vmem=400G
 # start program, if excecutable of course just call it
 module load conda
 source activate qpc_venv
-python /scratch/scc/santiago.salazar-jaramillo/hoping_tensors/scripts/QPC_dd_sim.py
+python /home/user/santiago.salazar-jaramillo/hoping_tensors/scripts/QPC_dd_sim.py
