ba272776df600f4af1bb6e917e04223e06300553
Ssalazar12 <slzr000@gmail.com>
2025-11-06 19:00:04 +0100

    addded a lot of new data and shuffled around the notebooks

--- scripts/MPS_run_dist.jl
+++ scripts/MPS_run_dist.jl
@@ -19,13 +19,13 @@ Strided.disable_threads()
 # PARAMETERS
 # ------------------------
 
-L_list = [100]
+L_list = [60]
 J_list  = [1.0] # qpc hopping
 t_list  = [0.001, 0.01, 0.03, 0.05, 0.07, 0.1, 0.3, 0.5, 0.8, 1.0, 2.0]  #qubit hopping
 Ω_list  = [0.3] # interaction 
 spread_list  = [6.0] # spread of the gaussian wavepacket
 K0_list  = [0.3*pi/2, 0.4*pi/2, 0.5*pi/2, 0.6*pi/2, 0.7*pi/2, 0.8*pi/2, 0.9*pi/2, pi/2] # group velocity of wavepacket
-X0_list  = [25] # 1 initial position of the wavepacket
+X0_list  = [11] # 1 initial position of the wavepacket
 Bindex_list  = ["half"] # can also be "half" to put in round(Int64, L/2) , 8 for exact comp
 t_step_list  = [0.07] # 
 ttotal_list  = ["fixed"] # can be set to fixed so it is equal to hit time of free particle
