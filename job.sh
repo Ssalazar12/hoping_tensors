7a9aa2ee46609eb6863ec29c522477fb0f878afc
Santiago Salazar Jaramillo <santiago.salazar-jaramillo@scc.physik.uni-konstanz.de>
2025-12-05 10:25:25 +0100

    much more new data for L=100 in exact diag

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@
 # folder MUST EXIST!
 #$ -e logs/ # give absolut path to the desired location
 #$ -o logs/
-#$ -l h_vmem=30G
+#$ -l h_vmem=40G
 # start program, if excecutable of course just call it
 module load conda
 source activate qpc_venv
